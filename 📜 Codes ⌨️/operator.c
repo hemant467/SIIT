08094ac7e015c4a3c2de1277eee6f6aded03f495
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-04-26 23:12:56 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/operator.c
@@ -0,0 +1,10 @@
+//Modulus operator
+int main()
+{
+	int a,b,remainder;
+	printf("Enter 2 numbers:");
+	scanf("%d%d",&a,&b);
+	
+	remainder = a%b;
+	printf("Remainder of %d divided by %d is: %d\n",a,b,remainder);
+}
