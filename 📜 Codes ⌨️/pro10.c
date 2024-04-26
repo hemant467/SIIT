c059ef52e247c22d003d6c2bedd159db13a49866
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-04-26 23:05:54 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/pro10.c
@@ -0,0 +1,23 @@
+//main()
+//{
+//	float p,t,r,si;
+//	printf("Enter p :");
+//	printf("Enter t :");
+//	printf("Enter r :");
+//	scanf("%f%f%f",&p,&t,&r);
+//	si=p*t*r/100; //Calculating average
+//	printf("simple intrest is %f",si);
+//}
+
+main()
+{
+	float p,t,r,si;
+	printf("Enter principle P :");
+	scanf("%f",&p);
+	printf("Enter Time t :");
+	scanf("%f",&t);
+	printf("Enter Rate r :");
+	scanf("%f",&r);
+	si=p*t*r/100; //Calculating average
+	printf("simple intrest is %f",si);
+}
