488b33d8630cfe31bf27f6e42306e4a2ce1f0b55
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-04-26 23:01:22 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/Example 1.c
@@ -0,0 +1,11 @@
+main()
+{
+	float x=0,y=0,result=0;
+	printf("Enter the value of x: ");
+	scanf("%f",&x);
+	printf("Enter the value of y: ");
+	scanf("%f",&y);
+	
+	result=x+y;
+	printf("sum of 2 numbers: %f",result);
+}
