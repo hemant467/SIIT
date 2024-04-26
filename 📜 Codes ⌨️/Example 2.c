c58f6a10ddc00c2de6b3ebc0c76485bf173ab098
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-04-26 23:10:33 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/Example 2.c
@@ -0,0 +1,9 @@
+main()
+{
+	int num1;
+	float num2;
+	char ch;
+	printf("Enter an integer a floating point number a character seperated by spaces: ");
+	scanf("%d %f %c", &num1,&num2,&ch);
+	printf("You have entered: %d, %.2f, and %c\n ",num1,num2,ch);
+}
