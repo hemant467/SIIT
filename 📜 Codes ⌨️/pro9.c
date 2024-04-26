3312c1652e8e0d2988e4dfafea0145782a0a1fed
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-04-26 23:05:25 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/pro9.c
@@ -0,0 +1,9 @@
+main()
+{
+	int a,b,c,d,e; //variable declaration
+	float avg;
+	printf("Enter 5 integers:");
+	scanf("%d%d%dd%d",&a,&b,&c,&d,&e);
+	avg=(a+b+c+d+e)/5.0; //Calculating average
+	printf("average of 5 subjects = %f",avg);
+}
