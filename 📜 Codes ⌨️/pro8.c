803a9c493efa7faa5d35485fcf78de1d8a9aeb55
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-04-26 23:05:00 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/pro8.c
@@ -0,0 +1,9 @@
+main()
+{
+	int a,b,c; //variable declaration
+	float avg;
+	printf("Enter 3 integers:");
+	scanf("%d%d%d",&a,&b,&c);
+	avg=(a+b+c)/3.0; //Calculating average
+	printf("average of 2 integers = %f",avg);
+}
