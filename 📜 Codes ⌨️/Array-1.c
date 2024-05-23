afb1008bffc642fc241d34911616abcbd45b16d6
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-05-23 21:42:19 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/Array-1.c
@@ -0,0 +1,11 @@
+main()
+{
+	int a[20],i,n;
+	printf("Enter n: ");
+	scanf("%d",&n);
+	printf("Enter elements: ");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
+	printf("%d ",a[i]);
+}
