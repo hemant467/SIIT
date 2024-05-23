29c4f65a993af79169609e6ee3803ab52b360a94
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-05-23 21:43:52 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/Assignment operator.c
@@ -0,0 +1,16 @@
+main()
+{
+	int a=5,b=6,c=7,d=8,e=9,f=10,g=2,h=5,i=20,x=8;
+	a+=2;
+	b-=4;
+	c*=4;
+	d/=5;
+	x%=6;
+	e&=5;
+	f|=6;
+	g^=7;
+	h<<=2;
+	i>>=3;
+	
+	printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d",a,b,c,d,e,f,g,h,i,x);
+}
