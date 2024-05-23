73b86d34272fc2efc8a556533588883f1f50801b
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-05-23 21:12:00 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/if-statement.c
@@ -0,0 +1,14 @@
+main()
+{
+	char ch;
+	printf("Enter a character: ");
+	scanf("%c",&ch);
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+	{
+		printf("%c is a Vowel\n",ch);
+	}
+		else
+			{
+				printf("%c is not a Vowel\n",ch);
+			}
+}
