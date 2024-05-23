ef3812cfce69d4a7960ad3cb0148d731445a6a40
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-05-23 21:26:18 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/prime numbers.c
@@ -0,0 +1,23 @@
+//Print prime numbers from 1 to n
+int main()
+{
+	int num,i,count,n;
+	printf("Enter range: ");
+	scanf("%d",&n);
+	
+	for(num=1;num<=n;num++)
+	{
+		count=0;
+		
+		for(i=2;i<=num/2;i++)
+		{
+			if(num%i==0)
+			{
+				count++;
+				break;
+			}
+		}
+		if(count==0 && num!=1)
+		printf("%d ",num);
+	}
+}
