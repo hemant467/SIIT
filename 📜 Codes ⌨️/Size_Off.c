8030b54d83dbc31455fb1fc7a7047f7908934b4c
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-05-23 21:30:03 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/Size_Off.c
@@ -0,0 +1,10 @@
+int main()
+{
+	printf("Size of char is %d byte\n", sizeof(char));
+	printf("Size of int is %d bytes\n", sizeof(int));
+	printf("Size of short int is %d bytes\n", sizeof(short int));
+	printf("Size of long int is %d bytes\n", sizeof(long int));
+	printf("Size of float is %d bytes\n", sizeof(float));
+	printf("Size of double is %d bytes\n", sizeof(double));
+	printf("Size of long double is %d bytes\n", sizeof(long double));
+}
