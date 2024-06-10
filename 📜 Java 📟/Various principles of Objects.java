6b75b8c895e5bb5872744aab456968f5c66ce4a5
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-06-10 21:01:18 +0530

    Add files via upload

---
+++ 📜 Java 📟/Various principles of Objects.java
@@ -0,0 +1,20 @@
+Class: A class is a collection of similar objects.
+
+Object : An object is a collection of members & methods. An instance of a class is also called as object.
+
+Various principles of Objects:
+1.Class
+
+2.Object
+
+3.Encapsulation = Hide text ; members & methods are bounced to protect from unauthorised user [PRIVATE]
+
+4.Abstraction = Class is an abstract datatype.
+
+5.Polymorphism = More than 1 operation {*Operator overloading  *Method overloading}
+
+6.Inheritance = 1 class acquire properties of another class.
+
+7.Dynamic binding
+
+8.Message communication
