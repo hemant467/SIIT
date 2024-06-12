06e02ac197d0b077c4ab3d94e29276a39874c51b
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-06-12 21:55:09 +0530

    Add files via upload

---
+++ 📜 Java 📟/Features of JAVA.java
@@ -0,0 +1,14 @@
+Features of JAVA : 
+
+1. High Security
+2. High Performance
+3. Architectural Neutral
+4. Platform Independent
+5. Portable
+6. Simple
+7. Object Oriented
+8. Robust [ Very STRONG ]
+9. Threaded
+10. Distributed
+11. Dynamic
+12. Buzz words of JAVA
