01c6cebcdf79f0673fb297d7ad879fc0c4c5ef1d
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-06-22 00:57:52 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/C++ Codes 📟/Area of rectangle.cpp
@@ -0,0 +1,11 @@
+//Find area of rectangle using C++
+using namespace std;
+#include<iostream>
+main()
+{
+	float a,b,l;
+	cout<<"enter l,b: ";
+	cin>>l>>b;
+	a=l*b;
+	cout<<"area of rectangle = "<<a;
+}
