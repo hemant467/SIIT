c500610cd2d09520ecedffaf4185bf8e5ded34e0
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-06-22 00:58:55 +0530

    Add files via upload

---
+++ 📜 Codes ⌨️/C++ Codes 📟/Even or Odd.cpp
@@ -0,0 +1,13 @@
+//To find Even or Odd using C++
+using namespace std;
+#include<iostream>
+main()
+{
+	int n;
+	cout<<"enter a number: ";
+	cin>>n;
+	if(n%2==0)
+	cout<<n<<" is EVEN";
+	else
+	cout<<n<<" is ODD";
+}
