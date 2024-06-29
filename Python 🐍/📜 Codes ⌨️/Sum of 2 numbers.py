091464f5fb07afa9a5cd58483c78ace56e947827
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-06-30 00:35:57 +0530

    Add files via upload

---
+++ Python 🐍/📜 Codes ⌨️/Sum of 2 numbers.py
@@ -0,0 +1,15 @@
+"""
+Sum of 2 numbers
+"""
+def sum(a, b):
+    return (a + b)
+
+a = float(input('Enter 1st number: '))
+b = float(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+"""
+Enter 1st number: 10.5
+Enter 2nd number: 19.5
+Sum of 10.5 and 19.5 is 30.0
+"""
