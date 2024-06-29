d503a9628e30f3e21448e26b26512357dd1a52aa
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-06-29 21:47:11 +0530

    Add files via upload

---
+++ 📜 Java 📟/📜 Codes ⌨️/wrapper.java
@@ -0,0 +1,17 @@
+package siit;
+
+public class wrapper {
+
+	public static void main(String ar[]) 
+	{
+		int a=5;
+		Integer p=5;
+		
+		Integer x=new Integer(5);
+		System.out.println(a);
+		System.out.println(p);
+		int m=p.intValue();
+		System.out.println(m);
+	}
+
+}
