c8681412038398ee0d81a67ee8b97e86dd1d8c21
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-06-29 21:44:38 +0530

    Add files via upload

---
+++ 📜 Java 📟/📜 Codes ⌨️/Area_of_circle.java
@@ -0,0 +1,12 @@
+package siit;
+
+public class Area_of_circle {
+
+	public static void main(String ar[]) 
+	{
+		double r=Double.parseDouble(ar[0]);
+		double a=Math.PI*r*r;
+		System.out.println(a);
+	}
+
+}
