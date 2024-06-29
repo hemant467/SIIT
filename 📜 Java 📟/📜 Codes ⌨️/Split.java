d03c66e99128461d09c46a1eb13417a9c54b10fb
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-06-29 21:46:50 +0530

    Add files via upload

---
+++ 📜 Java 📟/📜 Codes ⌨️/Split.java
@@ -0,0 +1,13 @@
+package siit;
+
+public class Split {
+
+	public static void main(String[] args) 
+	{
+		String s1="hello boys good students";
+		String a[]=s1.split(" ");
+		for(String i:a)
+			System.out.println(i);
+	}
+
+}
