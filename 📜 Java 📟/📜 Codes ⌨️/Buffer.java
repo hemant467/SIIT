69c8a664c6fce03ebbfbcd8e6cf2e85646583a81
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-07-01 18:37:10 +0530

    Add files via upload

---
+++ 📜 Java 📟/📜 Codes ⌨️/Buffer.java
@@ -0,0 +1,28 @@
+package siit;
+
+public class Buffer {
+
+	public static void main(String[] args) {
+		StringBuffer sb1=new StringBuffer("hello");
+		sb1.append("students");
+		sb1.append(25);
+		sb1.append(2.5);
+		System.out.println(sb1);
+		sb1.insert(5, "boys");
+		System.out.println(sb1);
+		sb1.delete(5, 9);
+		System.out.println(sb1);
+		sb1.replace(5, 13, "boys");
+		System.out.println(sb1);
+		System.out.println(sb1.reverse());
+		sb1.ensureCapacity(50);
+		System.out.println(sb1.capacity());
+	}
+}
+/*
+ * OUTPUT:
+ * hellostudents252.5
+ * helloboys252.5		-> sb1.reverse()
+ * 5.252syobolleh		-> sb1.reverse()
+ * 90					-> sb1.capacity()
+*/
