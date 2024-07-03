553a41e44983490ade6367886e79f3addb7e15f4
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-07-03 19:49:03 +0530

    Add files via upload

---
+++ 📜 Java 📟/📜 Codes ⌨️/Thread1.java
@@ -0,0 +1,21 @@
+package siit;
+
+public class Thread1 extends Thread 
+{
+
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i+=2)
+		{
+			System.out.println(i);
+			
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			}
+	
+}
