10b4bd36876d099d157acbe18df27f159ae1838b
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-07-04 19:52:11 +0530

    Add files via upload

---
+++ 📜 Java 📟/📜 Codes ⌨️/Calendaarr.java
@@ -0,0 +1,17 @@
+package siit;
+
+import java.util.Calendar;
+
+public class Calendaarr {
+
+	public static void main(String[] args) 
+	{
+		Calendar c= Calendar.getInstance();
+		System.out.println(c);
+
+	}
+
+}
+/* OUTPUT:
+ 			java.util.GregorianCalendar[time=1720091957208,areFieldsSet=true,areAllFieldsSet=true,lenient=true,zone=sun.util.calendar.ZoneInfo[id="Asia/Calcutta",offset=19800000,dstSavings=0,useDaylight=false,transitions=7,lastRule=null],firstDayOfWeek=1,minimalDaysInFirstWeek=1,ERA=1,YEAR=2024,MONTH=6,WEEK_OF_YEAR=27,WEEK_OF_MONTH=1,DAY_OF_MONTH=4,DAY_OF_YEAR=186,DAY_OF_WEEK=5,DAY_OF_WEEK_IN_MONTH=1,AM_PM=1,HOUR=4,HOUR_OF_DAY=16,MINUTE=49,SECOND=17,MILLISECOND=208,ZONE_OFFSET=19800000,DST_OFFSET=0]
+*/
